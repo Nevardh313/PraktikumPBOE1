8120021ba6ccf84ec94e589ad55d434193750c45
Nevardh <epeng5060@gmail.com>
2026-04-28 04:11:49 +0700

    Relasi antar objek

---
+++ Pertemuan3/src/MataKuliah.java
@@ -0,0 +1,55 @@
+/* Nama File    : MataKuliah.java
+ * Deskripsi    : berisi atribut dan method dalam class MataKuliah
+ * Pembuat      : Nevlyn Abbel Fikri Ardho
+ * Tanggal      : 5 Maret 2026
+*/
+
+public class MataKuliah {
+    private String idMatKul;
+    private String nama;
+    private int sks;
+
+    // Konstruktor tanpa parameter
+    public MataKuliah() {
+        this.idMatKul = "";
+        this.nama = "";
+        this.sks = 0;
+    }
+
+    // Konstruktor dengan parameter
+    public MataKuliah(String idMatKul, String nama, int sks) {
+        this.idMatKul = idMatKul;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    // Getter dan Setter
+    public String getIdMatKul() {
+        return idMatKul;
+    }
+
+    public void setIdMatKul(String idMatKul) {
+        this.idMatKul = idMatKul;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+
+    // Method untuk menampilkan data mata kuliah
+    public void tampilkan() {
+        System.out.println("ID: " + idMatKul + ", Nama: " + nama + ", SKS: " + sks);
+    }
+}
