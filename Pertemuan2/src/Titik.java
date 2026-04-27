44c3e5e2445bf5f284b18f9122243c9c38699a49
Nevardh <epeng5060@gmail.com>
2026-04-27 22:41:49 +0700

    Pertemuan2

---
+++ Pertemuan2/src/Titik.java
@@ -0,0 +1,38 @@
+public class Titik {
+    private double x;
+    private double y;
+
+    // Konstruktor default
+    public Titik() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // Konstruktor dengan parameter
+    public Titik(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getter dan Setter
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // Method untuk menampilkan titik
+    public void tampilkan() {
+        System.out.printf("(%.2f, %.2f)\n", x, y);
+    }
+}
