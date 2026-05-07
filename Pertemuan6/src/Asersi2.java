8cdebaff8e8424a3ea9d394157cd8c888c84b1b2
Nevardh <epeng5060@gmail.com>
2026-05-07 10:34:40 +0700

    Asersi & Eksepsi

---
+++ Pertemuan6/src/Asersi2.java
@@ -0,0 +1,27 @@
+/* Nama File    : Asersi2.java
+ * Deskripsi    : Program untuk menunjukkan asersi2
+ * Pembuat      : Nevlyn Abbel Fikri Ardho
+ * Tanggal      : 18 April 2026
+ */
+
+class Lingkaran {
+    private double jariJari;
+    public Lingkaran(double jariJari) {
+        this.jariJari = jariJari;
+    }
+    public double hitungKeliling() {
+        double keliling = 2 * Math.PI * jariJari;
+        return keliling;
+    }
+}
+
+public class Asersi2 {
+    public static void main(String[] args) {
+        double jariJari = 0;
+        assert (jariJari > 0) : "jari jari tidak boleh nol!!!";
+        Lingkaran l = new Lingkaran(jariJari);
+        double kelilingLingkaran = l.hitungKeliling();
+        System.out.println("keliling lingkaran = " 
+                + kelilingLingkaran);
+    }
+}
