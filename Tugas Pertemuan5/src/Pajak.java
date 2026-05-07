14da1ef61ef0b1d661e06de11c75ff62f5843f2d
Nevardh <epeng5060@gmail.com>
2026-05-07 08:56:47 +0700

    Tugas

---
+++ Tugas Pertemuan5/src/Pajak.java
@@ -0,0 +1,10 @@
+/* Nama File    : Pajak.java
+ * Deskripsi    : Membuat interface Pajak untuk menghitung pajak yang harus dibayar oleh Pengusaha dan Pekerja
+ * Pembuat      : Nevlyn Abbel Fikri Ardho
+ * Tanggal      : 16 April 2026
+ */
+
+public interface Pajak {
+    //Menghitung pajak yang harus dibayar
+    double hitungPajak();    
+}
