8566037ba78e76b67a87a5406690639e2cf358db
Nevardh <epeng5060@gmail.com>
2026-05-07 10:33:53 +0700

    Asersi & Eksepsi

---
+++ Pertemuan6/src/AngkaSialException.java
@@ -0,0 +1,11 @@
+/* Nama File    : AngkaSialException.java
+ * Deskripsi    : Exception angka 13 / angkasial
+ * Pembuat      : Nevlyn Abbel Fikri Ardho
+ * Tanggal      : 18 April 2026
+ */
+
+public class AngkaSialException extends Exception {
+    public AngkaSialException() {
+        super("jangan memasukkan angka 13 karena angka sial !!!");
+    }
+}
