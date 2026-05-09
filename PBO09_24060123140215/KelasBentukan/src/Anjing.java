2fd433e985909a1d48a531ddf51d2d3aba283736
Nevardh <epeng5060@gmail.com>
2026-05-09 19:21:30 +0700

    Koleksi

---
+++ PBO09_24060123140215/KelasBentukan/src/Anjing.java
@@ -0,0 +1,20 @@
+/* Nama File    : Anjing.java
+ * Deskripsi    : Membuat class Anjing sebagai subclass dari Anabul dengan mengoverride method gerak dan bersuara 
+                  untuk memberikan perilaku yang spesifik pada anjing. Method gerak akan menampilkan bahwa anjing 
+                  bergerak dengan melata, sedangkan method bersuara akan menampilkan bahwa anjing berbunyi guk-guk.
+ * Pembuat      : NEVLYN ABBEL FIKRI ARDHO / 24060123140215
+ * Tanggal      : 8 Mei 2026
+ */
+
+package KelasBentukan;
+
+public class Anjing extends Anabul {
+    public Anjing(String nama) {
+        super(nama);
+    }
+    
+    @Override
+    public void bersuara() {
+        System.out.println(nama + " menggonggong: Wuff! Wuff!");
+    }
+}
